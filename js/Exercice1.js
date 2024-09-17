5dadebaee3fa1d992f9f9976a9e22edb4ac127b3
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-17 16:46:02 -0700

    v0.5

---
+++ js/Exercice1.js
@@ -0,0 +1,22 @@
+// Calculator
+// Create a function per the following requirements:
+// Input (three parameters)
+// Number1, Number2, Mathematical Operation (+, -, *, /)
+// Return
+// The result of the mathematical operation in the following format:
+// Number1 operation Number2 = result.
+// Note:
+// Ask the user for the following
+//  Number1
+//  Number2
+//  Mathematical Operation (+, -, *, /)
+// Assume valid input
+// You can use three prompts or a single prompt (e.g. comma delimited string) or a
+// variation. Make sure it’s clear to the user what they need to enter.
+// Call your function with these values.
+// Display the string returned to the console.
+// Examples:
+// Input (can use multiple prompts): 1, 2, +
+// Return (note the format): 1 + 2 = 3
+// Input: 12, 3, /
+// Return: 12 / 3 = 4
