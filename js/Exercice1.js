7cb795dcf9aa575061e8c5fdaa7d87b80cd74dc5
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-18 10:36:14 -0700

    v1

--- js/Exercice1.js
+++ js/Exercice1.js
@@ -20,3 +20,50 @@
 // Return (note the format): 1 + 2 = 3
 // Input: 12, 3, /
 // Return: 12 / 3 = 4
+
+function Calculator(input) {
+  let int1 = Number.parseInt(input[0]);
+  let int2 = Number.parseInt(input[2]);
+  let operation = input[1];
+
+  if (Number.isInteger(int1)) {
+    if (Number.isInteger(int2)) {
+      switch (operation) {
+        case "+":
+          console.log(`${int1} + ${int2} = ${int1 + int2}`);
+          break;
+        case "-":
+          console.log(`${int1} - ${int2} = ${int1 - int2}`);
+          break;
+        case "*":
+          console.log(`${int1} * ${int2} = ${int1 * int2}`);
+          break;
+        case "%":
+          if (int2 !== 0) {
+            console.log(`${int1} % ${int2} = ${int1 % int2}`);
+          } else {
+            console.log("Division by 0 forbidden");
+          }
+          break;
+        case "/":
+          if (int2 !== 0) {
+            console.log(`${int1} / ${int2} = ${int1 / int2}`);
+          } else {
+            console.log("Division by 0 forbidden");
+          }
+          break;
+        default:
+          console.log("Operator has not been recognised");
+          break;
+      }
+    } else {
+      console.log("One of your numbers is not an integer less than 10");
+    }
+  } else {
+    console.log("One of your numbers is not an integer less than 10");
+  }
+}
+
+Calculator(
+  prompt("Type an operation with 2 integers less than 10 (e.g., 3*4)\nOperations accepted are: + - * / %")
+);
