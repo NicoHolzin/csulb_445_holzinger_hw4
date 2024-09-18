4b156ff887ebf2376b8398ca0c8b14d1aa38223c
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-18 11:28:30 -0700

    v2

--- js/Exercice2.js
+++ js/Exercice2.js
@@ -9,9 +9,12 @@
 // 212 Fahrenheit.
 // Celsius = (212 – 32) * 5/9 = 100
 // Below is the starter code (copy and paste the below and write the function)
-// A. Asher IS445/545 S01 – Fall 2024 Page 2 of 11
+
 // // Write code for a function
 // // TODO: your code here
+function fahrenheitToCelsius(tempF) {
+  return (((tempF-32)*5/9).toFixed(2));
+}
 // // Test your function.
 // // Note the following:
 // // Input: Fahrenheit temperature
@@ -21,17 +24,14 @@
 // // different formats. Therefore, the function returns the result and
 // // does not display it on the console.
 // // Create input and output variables and test
-// let deg212 = 212;
-// let converted212 = fahrenheitToCelsius(deg212);
-// console.log(`Water's boiling temperature is ${deg212} Fahrenheit or $
-// {converted212} Celsius`);
-// // Test with 32 degrees Fahrenheit (No variables)
-// console.log(`32 degrees Fahrenheit = ${fahrenheitToCelsius(32)} degrees
-// Celsius`);
-// // Create a variable and test
-// let deg98pt7 = 98.7;
-// console.log(`Temperature of ${deg98pt7} Fahrenheit is $
-// {fahrenheitToCelsius(deg98pt7)} degrees Celsius`);
+let deg212 = 212;
+let converted212 = fahrenheitToCelsius(deg212);
+console.log(`Water's boiling temperature is ${deg212} Fahrenheit or ${converted212} Celsius`);
+// Test with 32 degrees Fahrenheit (No variables)
+console.log(`32 degrees Fahrenheit = ${fahrenheitToCelsius(32)} degrees Celsius`);
+// Create a variable and test
+let deg98pt7 = 98.7;
+console.log(`Temperature of ${deg98pt7} Fahrenheit is ${fahrenheitToCelsius(deg98pt7)} degrees Celsius`);
 // Your output should be as follows:
 //  Water's boiling temperature is 212 Fahrenheit or 100 Celsius
 //  32 degrees Fahrenheit = 0 degrees Celsius
