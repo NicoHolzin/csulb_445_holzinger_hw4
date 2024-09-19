81900c60a615b5d4d8d04da9c9ea52ca1cdc8047
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-18 18:46:34 -0700

    v8

--- js/Exercice8.js
+++ js/Exercice8.js
@@ -7,3 +7,53 @@
 //  Correct
 // If correct, also display how many attempts it took. For example:
 // Correct! It took you 7 attempts to guess the correct number.
+
+// while (!Number.isInteger(int1)) {
+//   int1 = Number.parseInt(prompt("Type a number 1"));
+//   }
+function GameWithHelp() {
+  let Target = (Math.random()*100).toFixed();
+  let number;
+  let minGuess = 0;
+  let maxGuess = 100;
+  let attempsNb=0;
+  while((number=prompt(`Type a number\nMin guessed = ${minGuess}\nMax guessed = ${maxGuess}`))!==Target){
+    if (number<Target) {
+      console.log("Too low");
+      if (number>minGuess) {
+        minGuess=number;
+      }
+    }
+    if (number>Target) {
+      console.log("Too high");
+      if (number<maxGuess) {
+        maxGuess=number;
+      }
+    }
+    attempsNb++;
+  }
+  console.log(`Coorect, it took you ${attempsNb} attempts`);
+}
+
+function GameWithoutHelp() {
+  let Target = (Math.random()*100).toFixed();
+  let number;
+  let attempsNb=0;
+  while((number=prompt(`Type a number`))!==Target){
+    if (number<Target) {
+      console.log("Too low");
+    }
+    if (number>Target) {
+      console.log("Too high");
+    }
+    attempsNb++;
+  }
+  console.log(`Coorect, it took you ${attempsNb} attempts`);
+}
+
+let help = prompt("Do you want help ? ").toLowerCase()==="yes" ? true : false;
+if (help) {
+  GameWithHelp();
+} else {
+  GameWithoutHelp();
+} 
