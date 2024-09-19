c54de1278bb18656e6b06be1d8f24f9f17645006
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-18 17:42:59 -0700

    v4

--- js/Exercice4.js
+++ js/Exercice4.js
@@ -3,10 +3,51 @@
 //  Sum of all array values
 //  Minimum value
 //  Maximum value
-// const values = [3, 11, 7, 2, 9, 10];
+
 // Note:
 // Don't use array methods to compute the above.
 // Iterate through the array to compute the requested values.
 // The goal is for you to iterate through the array, we’ll discuss array methods in a later
 // lecture.
-// A. Asher IS445/545 S01 – Fall 2024 Page 4 of 11
+
+const values = [3, 11, 7, 2, 9, 10];
+
+function SumP(values) {
+  let sum=0;
+  for (const value of values) {
+    sum+=value;
+  }
+  return sum;
+}
+
+function MinP(values) {
+  let min=values[0];
+  for (const value of values) {
+    if (value<min) {
+      min=value;
+    }
+  }
+  return min;
+}
+
+function MaxP(values) {
+  let max=values[0];
+  for (const value of values) {
+    if (value>max) {
+      max=value;
+    }
+  }
+  return max;
+}
+
+const arrayProprieties = {
+  sum:SumP(values),
+  min:MinP(values),
+  max:MaxP(values),
+  describe() {console.log(`
+    The sum of the values is ${arrayProprieties.sum}\n
+    The min value is ${arrayProprieties.min}\n
+    The max value is ${arrayProprieties.max}`)}
+}
+
+arrayProprieties.describe();
