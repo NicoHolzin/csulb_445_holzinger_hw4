4e3a45c6bc6fefd0757f1ff6b67c9c5313641377
Nicolas Holzinger <nicoholzin@gmail.com>
2024-09-18 17:31:19 -0700

    v3

--- js/Exercice3.js
+++ js/Exercice3.js
@@ -2,7 +2,14 @@
 // Complete the following program to add the circle object definition. Its radius value is input
 // by the user.
 // Note: The circle object will have the radius attribute and two methods.
-// const r = Number(prompt("Enter the circle radius:"));
-// // TODO: create the circle object here
-// console.log(`Its circumference is ${circle.circumference()}`);
-// console.log(`Its area is ${circle.area()}`);
+
+const r = Number(prompt("Enter the circle radius:"));
+// TODO: create the circle object here
+const circle = {
+  radius: r,
+  circumference(){return 2*Math.PI*r},
+  area(){return Math.PI*r**2}
+}
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
+
